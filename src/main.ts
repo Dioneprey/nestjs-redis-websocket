66a4a2bad3727182196f99add6634b0a335d62d9
Dionatan Click Ativo <dionatan.goncalves@clickativo.com.br>
2024-10-18 17:57:00 -0300

    init redis setup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen('3333', '0.0.0.0').then(() => {
+    console.log(`[nestjs-redis-websocket] HTTP server running on port: 3333!`);
+  });
 }
 bootstrap();
